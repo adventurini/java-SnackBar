e3f81f40e728320d2125ff68761d5f1f9c13b21d
Anthony Venturini <anthonydventurini@gmail.com>
2019-07-08 13:10:33 -0700

    🚧 WIP MVP
    Adds class Snacks

---
+++ snackBar/Snacks.java
@@ -0,0 +1,51 @@
+package snackBar;
+
+public class Snacks {
+	private static int maxId = 0;
+	private int id;
+	
+	private int vendId;
+	private String name;
+	private int quantity;
+	private double cost;
+	
+	public int getVendId() {
+		return vendId;
+	}
+	
+	public void setVendId(int vendId) {
+		this.vendId=vendId;
+	}
+	
+	public Snacks(int vendId, String name, double cost, int quantity) {
+		maxId++;
+		this.id = maxId;
+		this.name = name;
+		this.cost = cost;
+		this.quantity = quantity;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void addQuantity(int quantity) {
+		this.quantity = quantity + this.quantity;
+	}
+	
+	public void buySnack(int quantity) {
+		this.quantity=this.quantity - quantity;
+	}
+	
+	public double getTotalCost(int quantity) {
+		return cost * quantity;
+	}
+}
