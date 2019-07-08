ff97b19ad788838d7aa99ba1d649de5c83c7aebf
Anthony Venturini <anthonydventurini@gmail.com>
2019-07-08 13:25:39 -0700

    ✨ Adds VendingMachines class
    Adds VendingMachines class

--- snackBar/Main.java
+++ snackBar/Main.java
@@ -1,10 +1,10 @@
-package snackBar;
-
-public class Main {
-	public static void main(String[] args) {
-		//This sets up two customers
-		Customer c1 = new Customer("Jane", 45.25);
-		Customer c2 = new Customer("Bob", 33.14);
-	}
-}
-
+//package snackBar;
+//
+//public class Main {
+//	public static void main(String[] args) {
+//		//This sets up two customers
+//		Customer c1 = new Customer("Jane", 45.25);
+//		Customer c2 = new Customer("Bob", 33.14);
+//	}
+//}
+//
